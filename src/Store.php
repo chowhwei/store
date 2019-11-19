f4631e5740b0a3abb553539d5e0816aa60d5e978
Yawl <chowhwei@gmail.com>
2019-11-19 23:00:17 +0800

    1.0.7

--- src/Store.php
+++ src/Store.php
@@ -4,6 +4,7 @@ namespace Chowhwei\Store;
 
 use Chowhwei\Store\Contracts\ConfigLoader as ConfigLoaderContract;
 use Chowhwei\Store\Store\KohanaConfigLoader;
+use Config;
 use Illuminate\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,7 +29,7 @@ class Store extends Container
          * 这里只在laravel外用，使用KohanaConfigLoader覆盖默认
          */
         $this->singleton(ConfigLoaderContract::class, function (Container $app) {
-            return new KohanaConfigLoader();
+            return new KohanaConfigLoader($app->make(Config::class));
         });
     }
 
@@ -36,7 +37,6 @@ class Store extends Container
      * @param string $name
      * @return Contracts\ContentStore
      * @throws \OSS\Core\OssException
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     static public function app(string $name){
         if(is_null(self::$app)){
--- src/Store/KohanaConfigLoader.php
+++ src/Store/KohanaConfigLoader.php
@@ -3,19 +3,27 @@
 namespace Chowhwei\Store\Store;
 
 use Chowhwei\Store\Contracts\ConfigLoader as ConfigLoaderContract;
-use Kohana;
+use Config;
+use Illuminate\Config\Repository;
 use Kohana_Exception;
-use Illuminate\Config\Repository as Config;
 
 class KohanaConfigLoader implements ConfigLoaderContract
 {
+    /** @var Config $config */
+    protected $config;
+
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+    }
+
     /**
-     * @param $file
-     * @return Config
+     * @param string $file
+     * @return Repository
      * @throws Kohana_Exception
      */
     public function load($file)
     {
-        return new Config((array)Kohana::$config->load($file));
+        return new Repository($this->config->load($file)->as_array());
     }
 }
